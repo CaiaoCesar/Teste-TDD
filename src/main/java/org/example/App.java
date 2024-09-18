5936d27075dca93e70191ee174ba669a605c5a26
Caio César <caiocesarc61@gmail.com>
2024-09-18 10:07:29 -0300

    Primeiro Commit do Teste TDD

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ * @author CaioCesar
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try (Scanner leitor = new Scanner(System.in)){
+             System.out.println( "Qual o valor: " );
+             String digito= leitor.next();
+             System.out.println(digito);
+        }
+    }
+
+    public void calculaNucleotideos(String arquivo){
+        return arrayInteiros;
+    }
+}
